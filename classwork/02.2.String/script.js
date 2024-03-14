7993086d633d1a204508432918933b01cdabcf96
FIRST_NAME LAST_NAME <ulfetzakirli@gmail.com>
2024-03-14 21:18:58 +0400

    feat: object

--- classwork/02.2.String/script.js
+++ classwork/02.2.String/script.js
@@ -37,6 +37,107 @@
 
 
 
+// let firstName = 'Xeyal Code Academy'
+
+// console.log(firstName[firstName.length - 1]);
+// console.log(firstName.at(-1));
+
+
+// console.log(firstName.slice(6));
+
+
+// console.log(firstName[1]);
+// console.log(firstName.at(-1));
+
+
+// concat
+// let country = 'Azerbaijan'
+// let city = 'Baku'
+
+
+// let myHome = country.concat(salary)
+// console.log(myHome);
+
+
+// let street = 'Niziami'
+
+// console.log(street.indexOf('i', 2));
+// console.log(street.lastIndexOf('i'));
+
+// let desc = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, assumenda.'
+
+// if (desc.toLowerCase().includes('ipsum doLoR i'.toLowerCase())) {
+//     console.log('this is data that you looking at');
+// } else {
+//     console.log('Sorry but not data that you want');
+// }
+
+
+// let city = 'Baku'
+
+// console.log(city.endsWith('o'));
+
+
+// let fullName = 'Ulfat Zakirli Rovshen'
+// console.log(fullName.split(" ")[1]);
+
+
+// concat, startsWith,endsWidth,indexOf,lastIndexOf,split
+
+
+
+
+// let posision = 'cybersecurty '
+// let salary = 2100
+// let result = posision.concat('nur', 'ayshan', 'ceycey', ' ', salary)
+// console.log(result);
+
+// let ayshan = 'cs302'
+// result = posision.toUpperCase().startsWith('C')
+// console.log(result)
+// console.log('ilqar'.endsWith('c'));
+
+
+// let rzayeva = 'newlesson'
+// console.log(rzayeva.endsWith('n'));
+
+// let info = 'durun gedin burdan'
+// console.log(info.split(' ')[1]);
+
+
+
+
+// ctrl + ?
+
+
+
+
+// let firstName = 'Aqil'
+
+// console.log(info.indexOf('sh'));
+// console.log(info.split('s'));
+
+
+// Aqil
+// Dasqin
+
+
+// let city = 'Baku'
+// console.log(firstName.concat('', 'Aqili', '56789', 'fghjk', city, 'ertyuio'));
+
+// includes()
+
+// console.log(city.includes('B'));
+
+
+// let address = 'sdfghjk'
+// console.log(address.replaceAll('a', 'o'));
+// console.log(address.length);
+
+
+
+
+
 
 
 
---
+++ classwork/03.Object/script.js
@@ -0,0 +1,51 @@
+// Object
+// let firstName = 'Ulfat'
+// let age = '23'
+// let lastName = 'Zakirli'
+// let address = 'Baku'
+// let uni = 'ADNSU'
+
+// const obj1 = new Object() //constructor
+// const obj2 = {} //literal
+
+const user = {
+    firstName: 'Ulfat',
+    lastName: 'Zakirli',
+    age: 23,
+    address: {
+        city: 'Baku',
+        country: 'Az',
+        street: 'Nizami 23',
+        some: {
+            l: '55',
+            b: '343'
+        }
+    },
+    uni: "ADNSU",
+    isAdmin: false,
+    salary: null,
+    position: undefined
+}
+console.log(user);
+
+
+user.test = 'I am test'
+// console.log('1', user);
+// delete user.firstName
+// user.street = 'Nizami 23'
+// console.log('2', user);
+// user.position = 'MERN'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
