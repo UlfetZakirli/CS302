8a025969e576d4550413384bb6eeda46aad68d59
FIRST_NAME LAST_NAME <ulfetzakirli@gmail.com>
2024-03-28 20:53:19 +0400

    feat: Form

--- classwork/07.Form/script.js
+++ classwork/07.Form/script.js
@@ -5,13 +5,20 @@ const email = document.querySelector('#email')
 const password = document.querySelector('#password')
 const form = document.querySelector('#userForm')
 
+const db = []
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
-    console.log(username.value);
-    console.log(email.value);
-    console.log(password.value);
+    const user = {
+        username: username.value,
+        email: email.value,
+        password: password.value
+    }
+    db.push(user)
+    console.log('database', db);
 })
+
+
 
 
 
